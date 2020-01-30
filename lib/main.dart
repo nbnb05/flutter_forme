caad1e54fc6b1c99a53550746d9fd1de402082e3
nb <beaumier.noel@wanadoo.fr>
2020-01-30 17:53:29 +0100

    rajout d'un bouton

--- lib/main.dart
+++ lib/main.dart
@@ -123,26 +123,36 @@ class MyCustomFormState extends State<MyCustomForm> {
   // not a GlobalKey<MyCustomFormState>.
   final _formKey = GlobalKey<FormState>();
 
-  @override // fonction qui existe 
+  @override // fonction qui existe
   Widget build(BuildContext context) {
     // Build a Form widget using the _formKey created above.
     return Form(
-      key: _formKey,
-      child: Column(
-        children: <Widget>[TextFormField(
-  // The validator receives the text that the user has entered.
-  validator: (value) {
-    if (value.isEmpty) {
-      return 'Please enter some text';
-    }
-    return null;
-  },
-)
+        key: _formKey,
+        child: Column(children: <Widget>[
+          TextFormField(
+            // The validator receives the text that the user has entered.
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter text';
+              }
+              return null;
+            },
+          ),
+          RaisedButton(
+            onPressed: () {
+              // Validate returns true if the form is valid, otherwise false.
+              if (_formKey.currentState.validate()) {
+                // If the form is valid, display a snackbar. In the real world,
+                // you'd often call a server or save the information in a database.
 
-              // Add TextFormFields and RaisedButton here.
-        ]
-     )
-    );
+                Scaffold.of(context)
+                    .showSnackBar(SnackBar(content: Text('Processing Data')));
+              }
+            },
+            child: Text('Submit'),
+          )
+
+          // Add TextFormFields and RaisedButton here.
+        ]));
   }
 }
-
